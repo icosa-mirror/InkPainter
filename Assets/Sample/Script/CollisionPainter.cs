ddbc9d5744d1d7141f6caef9f24adfdc95d1d79e
Andy Baker <andy@andybak.net>
2024-05-22 21:42:32 +0100

    Global reformat

--- Assets/Sample/Script/CollisionPainter.cs
+++ Assets/Sample/Script/CollisionPainter.cs
@@ -2,39 +2,37 @@
 
 namespace Es.InkPainter.Sample
 {
-	[RequireComponent(typeof(Collider), typeof(MeshRenderer))]
-	public class CollisionPainter : MonoBehaviour
-	{
-		[SerializeField]
-		private Brush brush = null;
+    [RequireComponent(typeof(Collider), typeof(MeshRenderer))]
+    public class CollisionPainter : MonoBehaviour
+    {
+        [SerializeField] private Brush brush = null;
 
-		[SerializeField]
-		private int wait = 3;
+        [SerializeField] private int wait = 3;
 
-		private int waitCount;
+        private int waitCount;
 
-		public void Awake()
-		{
-			GetComponent<MeshRenderer>().material.color = brush.Color;
-		}
+        public void Awake()
+        {
+            GetComponent<MeshRenderer>().material.color = brush.Color;
+        }
 
-		public void FixedUpdate()
-		{
-			++waitCount;
-		}
+        public void FixedUpdate()
+        {
+            ++waitCount;
+        }
 
-		public void OnCollisionStay(Collision collision)
-		{
-			if(waitCount < wait)
-				return;
-			waitCount = 0;
+        public void OnCollisionStay(Collision collision)
+        {
+            if (waitCount < wait)
+                return;
+            waitCount = 0;
 
-			foreach(var p in collision.contacts)
-			{
-				var canvas = p.otherCollider.GetComponent<InkCanvas>();
-				if(canvas != null)
-					canvas.Paint(brush, p.point);
-			}
-		}
-	}
+            foreach (var p in collision.contacts)
+            {
+                var canvas = p.otherCollider.GetComponent<InkCanvas>();
+                if (canvas != null)
+                    canvas.Paint(brush, p.point);
+            }
+        }
+    }
 }
